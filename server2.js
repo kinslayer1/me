64e2737a0f2952440de46552c26d2b8cfc242ccd
kinslayer1 <jairo0914@gmail.com>
2019-05-24 15:54:31 -0500

    Create server2.js

---
+++ server2.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var http = require('http')
+var app = express()
+ 
+var users = ['oscar', 'juan', 'marcos']
+ 
+app.get('/users', (req, res) => {
+  res.send(users)
+})
+ 
+app.get('/', (req, res) => {
+  res.status(200).send("Welcome to API REST")
+})
+ 
+http.createServer(app).listen(8001, () => {
+  console.log('Server started at http://localhost:8001');
+});
